b8f2099625016f2ec6686532cfd39d1a535d1dc7
Rishabh <rishabhbhatt956@gmail.com>
2025-05-24 16:56:51 +0530

    feat: Implement user model with authentication methods and database connection (PENDING) (STARTING WITH CONTROLLERS)

--- Uber/Backend/app.js
+++ Uber/Backend/app.js
@@ -3,8 +3,10 @@ dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
+const connectToDb = require("./db/db");
 app.use(cors());
+
+connectToDb();
 
 app.get("/", (req, res) => {
   res.send("Helllo world");
