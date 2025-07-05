4b171605f0dab4769feaac34d252a3e5fd25c5d8
Rishabh <rishabhbhatt956@gmail.com>
2025-07-05 11:18:51 +0530

    Add CaptainDetails component and integrate it into CaptainHome layout

--- Uber/frontend/src/pages/CaptainHome.jsx
+++ Uber/frontend/src/pages/CaptainHome.jsx
@@ -1,56 +1,26 @@
 import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import CaptainDetails from '../components/CaptainDetails'
 
 const CaptainHome = () => {
-  const location = useLocation()
-    const { ride } = location.state || {} 
+//   const location = useLocation()
+    // const { ride } = location.state || {} 
   return (
-    <div className='h-screen'>
-          <div className='fixed p-3 top-0 mt-6 flex items-center justify-center'>
+    <div className='h-screen overflow-hidden relative'>
+          <div className='fixed p-3 top-0 mt-6 flex items-center justify-between w-screen'>
             <img className='w-16 ' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
-            <Link to='/home' className='fixed right-2 top-0 mt-6 h-10 w-10 bg-white flex items-center justify-center rounded-full'>
+            <Link to='/captain-home' className='fixed right-2 top-0 mt-6 h-10 w-10 bg-white flex items-center justify-center rounded-full'>
                 <i className="text-lg font-medium ri-logout-box-r-line"></i>
             </Link>
           </div>
-            <div className='h-1/2'>
+            <div className='h-3/5'>
                 <img src="https://preview.redd.it/uber-doesnt-show-me-estimated-prices-and-hasnt-for-months-v0-2601fzw9tlwb1.jpg?width=640&crop=smart&auto=webp&s=ea3ac677b73a805880377ddd59b9e2e5fea5454a" alt="" />
 
             </div>
-            {/* <div className='h-1/2'> */}
-                {/* <LiveTracking /> */}
-
-            {/* </div> */}
-            <div className='h-1/2 p-4'>
-                <div className='flex items-center justify-between'>
-                    <img className='h-12' src="https://swyft.pl/wp-content/uploads/2023/05/how-many-people-can-a-uberx-take.jpg" alt="" />
-                    <div className='text-right'>
-                        <h2 className='text-lg font-medium capitalize'>{ride?.captain.fullname.firstname}</h2>
-                        <h4 className='text-xl font-semibold -mt-1 -mb-1'>{ride?.captain.vehicle.plate}</h4>
-                        <p className='text-sm text-gray-600'>Maruti Suzuki Alto</p>
-
-                    </div>
+            <div className='bg-white absolute h-full w-full'>
+                <div className='h-2/5 p-4 '>
+                    <CaptainDetails/>
                 </div>
-
-                <div className='flex gap-2 justify-between flex-col items-center'>
-                    <div className='w-full mt-5'>
-
-                        <div className='flex items-center gap-5 p-3 border-b-2'>
-                            <i className="text-lg ri-map-pin-2-fill"></i>
-                            <div>
-                                <h3 className='text-lg font-medium'>562/11-A</h3>
-                                <p className='text-sm -mt-1 text-gray-600'>{ride?.destination}</p>
-                            </div>
-                        </div>
-                        <div className='flex items-center gap-5 p-3'>
-                            <i className="ri-currency-line"></i>
-                            <div>
-                                <h3 className='text-lg font-medium'>₹{ride?.fare} </h3>
-                                <p className='text-sm -mt-1 text-gray-600'>Cash Cash</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
             </div>
         </div>
   )
