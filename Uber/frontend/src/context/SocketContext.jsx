ed6d949a87b6b55880f47cb03537dd7d7ad0d50c
Rishabh <rishabhbhatt956@gmail.com>
2025-07-25 08:19:14 +0530

    feat: add SocketProvider for real-time communication and integrate it into the main application

---
+++ Uber/frontend/src/context/SocketContext.jsx
@@ -0,0 +1,39 @@
+import React, { createContext, useContext, useEffect, useRef } from "react";
+import { io } from "socket.io-client";
+
+export const SocketContext = createContext();
+
+const socket = io(`${import.meta.env.VITE_BASE_URL}`)
+
+const SocketProvider = ({ children }) => {
+//   const socketRef = useRef();
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Connected to Socket.IO server");
+    })
+
+    socket.on("disconnect", () => {
+      console.log("Disconnected from Socket.IO server");
+    })
+
+  }, []);
+
+  // Send a message to a specific event
+  const sendMessage = (eventName, message) => {
+    socket.emit(eventName, message);
+  };
+
+  // Listen for messages from a specific event
+  const receiveMessage = (eventName, callback) => {
+    socket.on(eventName, callback);
+  };
+
+  return (
+    <SocketContext.Provider value={{ sendMessage, receiveMessage}}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
+
+export default SocketProvider;
