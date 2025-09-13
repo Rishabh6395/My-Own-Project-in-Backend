11a4975b23629fa9534273e46ef17f1058eae8d8
Rishabh <rishabhbhatt956@gmail.com>
2025-09-13 17:28:02 +0530

    Refactor code structure for improved readability and maintainability

--- Zomato with AI/backend/src/controllers/food.controller.js
+++ Zomato with AI/backend/src/controllers/food.controller.js
@@ -1,9 +1,26 @@
 const foodModel = require('../models/food.model');
+const storageService = require('../services/storage.service');
+const {v4: uuid} = require("uuid")
 
 async function createFood(req, res) {
 
-    console.log(req.foodpartner);
 
+    const fileUploadResult = await storageService.uploadFile(req.file.buffer, uuid());
+
+    const foodItem = await foodModel.create({
+        name: req.body.name,
+        description: req.body.description,
+        video: fileUploadResult.url,
+        price: req.body.price,
+        image: fileUploadResult.url,
+        category: req.body.category,
+        foodpartner: req.foodpartner._id
+    })
+
+    res.status(201).json({
+        message: "Food item created",
+        food: foodItem
+    });
 }
 
 module.exports = {
--- Zomato with AI/backend/src/routes/food.route.js
+++ Zomato with AI/backend/src/routes/food.route.js
@@ -2,8 +2,16 @@ const express = require('express');
 const foodCountroller = require('../controllers/food.controller');
 const aurthMiddleware = require('../middlewares/auth.middleware');  
 const router = express.Router();
+const multer = require('multer');
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+})
 
 // add protected route
-router.post('/', aurthMiddleware.authFoodPartnerMiddleware, foodCountroller.createFood);
+router.post('/',
+    aurthMiddleware.authFoodPartnerMiddleware, 
+    upload.single('Video'), 
+    foodCountroller.createFood);
 
 module.exports = router;
